e7ba8cfbd32e43935db71658545bf0f8e6254bd7
Sergey Petrinich <petrinichshadow@gmail.com>
2022-05-24 19:44:08 +0300

    header v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { lazy, Suspense, useEffect } from "react";
+import * as flsFunctions from "./js/files/functions";
+import dynamicAdaptive from "./js/libs/dynamic_adapt";
+// import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+
 import { Header, Footer } from "./_containers";
+// import { Spinner } from "./_components";
+
+// const Page404 = lazy(() => import("./_pages/Page404")),
+//     Home = lazy(() => import("./_pages/Home")),
+//     About = lazy(() => import("./_pages/About")),
+//     Career = lazy(() => import("./_pages/Career")),
+//     Contacts = lazy(() => import("./_pages/Contacts")),
+//     Products = lazy(() => import("./_pages/Products"));
+
+// const ScrollToTop = () => {
+//     const { pathname } = useLocation();
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//         flsFunctions.menuClose();
+//     }, [pathname]);
+//     return null;
+// };
 
 const App = () => {
+    useEffect(() => {
+        flsFunctions.menuInit();
+        dynamicAdaptive();
+    }, []);
+
     return (
         <>
             <Header />
--- src/_containers/Header.jsx
+++ src/_containers/Header.jsx
@@ -1,68 +1,69 @@
 import React from "react";
+import { images } from "../constants";
+
+const MenuItem = ({ title, link }) => {
+    return (
+        <li className="menu__item">
+            <a href={link} className="menu__link">
+                {title}
+            </a>
+        </li>
+    );
+};
+
+const ActionsHeaderItem = ({ title, link, img, alt }) => {
+    return (
+        <li className="actions-header__item">
+            <a href={link} className="actions-header__link">
+                <div className="actions-header__icon">
+                    <img src={img} alt={alt} />
+                </div>
+                <span>{title}</span>
+            </a>
+        </li>
+    );
+};
 
 const Header = () => {
+    const { icons, defaultImages } = images;
+
     return (
         <header className="header">
             <div className="header__top top-header">
-                <div className="top-header__content _container">
+                <div className="top-header__container">
                     <div className="top-header__column">
                         <div className="top-header__menu menu">
-                            <div className="menu__icon icon-menu">
+                            <button type="button" className="menu__icon icon-menu">
                                 <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
+                            </button>
                             <nav className="menu__body">
                                 <ul className="menu__list">
-                                    <li>
-                                        <a href="#" className="menu__link">
-                                            Бренды
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="menu__link">
-                                            Распродажа
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="menu__link">
-                                            Доставка и Сборка
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="menu__link">
-                                            Гарантия
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="menu__link">
-                                            Оплата
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="menu__link">
-                                            Контакты
-                                        </a>
-                                    </li>
+                                    <MenuItem title="Бренды" link="#" />
+                                    <MenuItem title="Распродажа" link="#" />
+                                    <MenuItem title="Доставка и Сборка" link="#" />
+                                    <MenuItem title="Гарантия" link="#" />
+                                    <MenuItem title="Оплата" link="#" />
+                                    <MenuItem title="Контакты" link="#" />
                                 </ul>
                             </nav>
                         </div>
                     </div>
                     <div className="top-header__column">
                         <a href="#" className="top-header__logo">
-                            <img src="img/main/header/logo.png" alt="logo" />
+                            <img src={defaultImages.logo} alt="Логотип" />
                         </a>
                     </div>
                     <div className="top-header__column ">
                         <div className="top-header__contacts contacts-header">
-                            <div className="contacts-header__column">
+                            <a href="tel:84991234567" className="contacts-header__column">
+                                <div className="contacts-header__icon">
+                                    <img src={icons.phone_2} alt="Позвонить" />
+                                </div>
                                 <div className="contacts-header__item contacts-header__item_icon">
                                     <div className="contacts-header__label">Москва</div>
-                                    <a href="tel:84991234567" className="contacts-header__phone">
-                                        8-499-123-45-67
-                                    </a>
+                                    <span className="contacts-header__phone">8-499-123-45-67</span>
                                 </div>
-                            </div>
+                            </a>
                             <div className="contacts-header__column contacts-header__column_forcart">
                                 <div className="contacts-header__item contacts-header__item_second-phone">
                                     <div className="contacts-header__label">Бесплатно для России</div>
@@ -75,36 +76,36 @@ const Header = () => {
                     </div>
                 </div>
             </div>
+
             <div className="header__bottom bottom-header">
-                <div className="bottom-header__container _container">
+                <div className="bottom-header__container ">
                     <div className="bottom-header__row">
                         <div className="bottom-header__column">
-                            <ul data-da=".menu__body,640,0" className="bottom-header__actions actions-header">
-                                <li>
-                                    <a href="#" className="actions-header__item actions-header__item_login">
-                                        <span>Вход</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" className="actions-header__item actions-header__item_reg">
-                                        <span>Регистрация</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="mailto:sport@gmail.com" className="actions-header__item actions-header__item_email">
-                                        <span>sport@gmail.com</span>
-                                    </a>
-                                </li>
+                            <ul data-da=".menu__body,640,last" className="bottom-header__actions actions-header">
+                                <ActionsHeaderItem title="Вход" link="#" img={icons.key} alt="Вход" />
+                                <ActionsHeaderItem title="Регистрация" link="#" img={icons.head} alt="Регистрация" />
+                                <ActionsHeaderItem
+                                    title="sport@gmail.com"
+                                    link="mailto:sport@gmail.com"
+                                    img={icons.mail}
+                                    alt="Почта"
+                                />
                             </ul>
                         </div>
                         <div className="bottom-header__column">
                             <div className="bottom-header__info info-header">
                                 <div className="info-header__column">
-                                    <a href="#" data-da=".menu__body,640,2" className="info-header__callback">
+                                    <a href="#" data-da=".menu__body,640,last" className="info-header__callback">
+                                        <div className="info-header__icon">
+                                            <img src={icons.phone_3} alt="Обратный звонок" />
+                                        </div>
                                         <span>Обратный звонок</span>
                                     </a>
                                 </div>
-                                <div className="info-header__column">
+                                <div className="info-header__column info-header__column_phone">
+                                    <div className="info-header__icon">
+                                        <img src={icons.date} alt="График работы" />
+                                    </div>
                                     <div className="info-header__schedule">
                                         <p>
                                             Пн–Пт:<span> 09:00–21:00</span>
@@ -115,7 +116,10 @@ const Header = () => {
                                     </div>
                                 </div>
                                 <div className="info-header__column">
-                                    <a data-da=".contacts-header__column_forcart,640,2" href="#" className="info-header__cart">
+                                    <a data-da=".contacts-header__column_forcart,640,last" href="#" className="info-header__cart">
+                                        <div className="info-header__cart-icon">
+                                            <img src={icons.cart_2} alt="Корзина" />
+                                        </div>
                                         <span>12</span>
                                     </a>
                                 </div>
@@ -123,6 +127,10 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="header__bg">
+                <img src={defaultImages.bg_header} alt="Задний фон" />
             </div>
         </header>
     );
