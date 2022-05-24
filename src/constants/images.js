5d00915a4693afa2eb0830388838fe38a3e2b07e
Sergey Petrinich <petrinichshadow@gmail.com>
2022-05-24 17:05:27 +0300

    added images to constants / index.html updated

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,186 @@
-import bg from "./../assets/img/bg.jpg";
-
-const defaultImages = {
-    bg,
+import arrow from "./../img/_icons/arrow.png";
+import arrow_li from "./../img/_icons/arrow_li.png";
+import arrow_nav from "./../img/_icons/arrow_nav.png";
+import arrow_nav_hv from "./../img/_icons/arrow_nav_hv.png";
+import arrow_search from "./../img/_icons/arrow_search.png";
+import arrow_spoller from "./../img/_icons/arrow_spoller.png";
+import arrow_spoller_hv from "./../img/_icons/arrow_spoller_hv.png";
+import arrow_sl from "./../img/_icons/arrow_sl.svg";
+import arrow_sl_hv from "./../img/_icons/arrow_sl_hv.svg";
+import cart_1 from "./../img/_icons/cart_1.png";
+import cart_2 from "./../img/_icons/cart_2.png";
+import cart_3 from "./../img/_icons/cart_3.png";
+import compare_1 from "./../img/_icons/compare_1.png";
+import compare_2 from "./../img/_icons/compare_2.png";
+import cross from "./../img/_icons/cross.png";
+import cross_hv from "./../img/_icons/cross_hv.png";
+import date from "./../img/_icons/date.png";
+import delivery from "./../img/_icons/delivery.png";
+import head from "./../img/_icons/head.png";
+import tick from "./../img/_icons/tick.png";
+import repair from "./../img/_icons/repair.png";
+import items_grid from "./../img/_icons/items_grid.png";
+import items_list from "./../img/_icons/items_list.png";
+import key from "./../img/_icons/key.png";
+import mail from "./../img/_icons/mail.png";
+import phone_1 from "./../img/_icons/phone_1.png";
+import phone_2 from "./../img/_icons/phone_2.png";
+import phone_3 from "./../img/_icons/phone_3.png";
+import remove from "./../img/_icons/remove.png";
+import remove_hv from "./../img/_icons/remove_hv.png";
+import search from "./../img/_icons/search.png";
+import trash from "./../img/_icons/trash.png";
+import triangle from "./../img/_icons/triangle.png";
+const icons = {
+    arrow,
+    arrow_li,
+    arrow_nav,
+    arrow_nav_hv,
+    arrow_search,
+    arrow_spoller,
+    arrow_spoller_hv,
+    arrow_sl,
+    arrow_sl_hv,
+    cart_1,
+    cart_2,
+    cart_3,
+    compare_1,
+    compare_2,
+    cross,
+    cross_hv,
+    date,
+    delivery,
+    head,
+    tick,
+    repair,
+    items_grid,
+    items_list,
+    key,
+    mail,
+    phone_1,
+    phone_2,
+    phone_3,
+    remove,
+    remove_hv,
+    search,
+    trash,
+    triangle,
 };
 
-export default { defaultImages };
+import catalog01 from "./../img/catalog/01.jpg";
+import catalog02 from "./../img/catalog/02.jpg";
+import catalog03 from "./../img/catalog/03.jpg";
+import catalogBg from "./../img/catalog/bg.png";
+const catalogImages = {
+    catalog01,
+    catalog02,
+    catalog03,
+    catalogBg,
+};
+
+import payCard01 from "./../img/payCards/01.png";
+import payCard02 from "./../img/payCards/02.png";
+import payCard03 from "./../img/payCards/03.png";
+import payCard04 from "./../img/payCards/04.png";
+import payCard05 from "./../img/payCards/05.png";
+const payCardsimages = {
+    payCard01,
+    payCard02,
+    payCard03,
+    payCard04,
+    payCard05,
+};
+
+import vkSoclial from "./../img/social/01.png";
+import fbSocial from "./../img/social/02.png";
+import instSocial from "./../img/social/03.png";
+import okSocial from "./../img/social/04.png";
+const socialIcons = {
+    vkSoclial,
+    fbSocial,
+    instSocial,
+    okSocial,
+};
+
+import delivery01 from "./../img/delivery/01.png";
+import delivery02 from "./../img/delivery/02.png";
+import delivery03 from "./../img/delivery/03.png";
+import delivery04 from "./../img/delivery/04.png";
+import delivery05 from "./../img/delivery/05.png";
+import delivery06 from "./../img/delivery/06.png";
+const deliveryImages = {
+    delivery01,
+    delivery02,
+    delivery03,
+    delivery04,
+    delivery05,
+    delivery06,
+};
+
+import mainSlide01 from "./../img/mainSlider/01.jpg";
+import mainSlide02 from "./../img/mainSlider/02.jpg";
+import mainSlide03 from "./../img/mainSlider/03.jpg";
+import mainSlide04 from "./../img/mainSlider/04.jpg";
+import mainSlide05 from "./../img/mainSlider/05.jpg";
+const mainSlidesImages = {
+    mainSlide01,
+    mainSlide02,
+    mainSlide03,
+    mainSlide04,
+    mainSlide05,
+};
+
+import partnerLogo01 from "./../img/partners/01.png";
+import partnerLogo02 from "./../img/partners/02.png";
+import partnerLogo03 from "./../img/partners/03.png";
+import partnerLogo04 from "./../img/partners/04.png";
+import partnerLogo05 from "./../img/partners/05.png";
+const partnersLogoImages = {
+    partnerLogo01,
+    partnerLogo02,
+    partnerLogo03,
+    partnerLogo04,
+    partnerLogo05,
+};
+
+import productImage01 from "./../img/product/01.jpg";
+import productImage02 from "./../img/product/02.jpg";
+import productImage03 from "./../img/product/03.jpg";
+import productImage04 from "./../img/product/04.jpg";
+import productImage05 from "./../img/product/05.jpg";
+const productImages = {
+    productImage01,
+    productImage02,
+    productImage03,
+    productImage04,
+    productImage05,
+};
+
+import bg from "./../img/bg.jpg";
+import bg_side from "./../img/bg_side.jpg";
+import bg_header from "./../img/bg_header.jpg";
+import logo from "./../img/logo.png";
+import logo_footer from "./../img/logo_footer.png";
+import woman from "./../img/woman.png";
+import vidget from "./../img/vidget.png";
+const defaultImages = {
+    bg,
+    bg_side,
+    bg_header,
+    logo,
+    logo_footer,
+    woman,
+    vidget,
+};
+
+export default {
+    icons,
+    catalogImages,
+    payCardsimages,
+    socialIcons,
+    deliveryImages,
+    mainSlidesImages,
+    partnersLogoImages,
+    productImages,
+    defaultImages,
+};
